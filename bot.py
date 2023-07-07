223a3c1e12bbf206306e8bb2c621780f0826b04b
Bill Kaminski <billykaminski123@gmail.com>
2023-07-07 13:13:41 -0400

    functioning bot

--- bot.py
+++ bot.py
@@ -1,22 +1,30 @@
 import discord
+import os
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
-from pprint import pprint
 import re
-import schedule
-import time
+from discord.ext import commands
+import asyncio
 
 
+load_dotenv()
 # initialize an HTTP session
 session = HTMLSession()
 
-def go_to_bungo(url):
-    res = session.get(url)
-    soup = soup = BeautifulSoup(res.html.html, "html.parser")
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Create an instance of the bot
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+async def find_tweet(url):
     
-    links = []
-    times = []
-    divs = soup.find_all('div', {"class":"fy7gGf"})
+    res = session.get(url)
+    soup = BeautifulSoup(res.html.html, "html.parser")
+    
+    links = set()
+    divs = (soup.find_all('div', {"class":"fy7gGf"}))
     for div in divs:
         if div.find("a", {"class":"h4kbcd"}):
             #paragraphs = [p.text for p in soup.find_all('p')]
@@ -24,53 +32,31 @@ def go_to_bungo(url):
             regex = re.compile('.·')
             #remove · character from time list, this is the only way I could think to remove it
             time = [i for i in time_tags if not regex.match(i)]
-            #print(clean_time)
-            #print(clean_time[0][0])
-            print(time[0])
-            if 'hours' in time[0] and int(time[0][0]) > 1 or 'mins' in time[0]:
+            #if 'hour' in time[0] and int(time[0][0]) > 1 or 'mins' in time[0]:
+            if 'day' in time[0] and int(time[0][0]) >= 1 or 'mins' in time[0]:
                 #times.append(time[0])
                 link = div.find("a")
-                links.append( link['href'] )
-            
+                links.add( link['href'] )
+    #pprint(list(links))
+    return links
     
-    #times = list(set(times))
-    links = list(set(links))
+@bot.command(pass_context=True)
+async def send_array_contents(array):
+    channel = bot.get_channel(1126904064119152771) 
+  
+    for item in array:
+        print(item)
+        #await channel.send(item)
 
-    #pprint(times)
-    #pprint(links)
-    
-    #pprint(res)
-
-    #for div in divs_w_link:
-    #    time = div.find_all("span", {"class":"f"})
-    #    print(time)
-    #print(divs_w_link)
-    #print("Link Text:", link_text)
-    #print("Href Attribute:", href)
-    '''client = discord.client()
-    @client.event
-    async def on_ready():
-        print('We have logged in as {0.user}'.format(client))'''
-    
-
-# Schedule the function call every hour
-schedule.every().hour.do( go_to_bungo('https://www.google.com/search?q=bungiehelp') )
-
-# Run the scheduled task indefinitely
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-
-
-    '''links = soup.find_all("a", {"class": "h4kbcd"})
-    for link in links:
-        #print(link, "\n")
-        res = session.get(link['href'])
-        soup = BeautifulSoup(res.html.html, "html.parser")
-        print(soup.find_all("a"), "\n")
+async def message():
+    links = await find_tweet('https://www.google.com/search?q=bungiehelp')
+    await send_array_contents(links)
         
-        html = driver.page_source
-        soup = BeautifulSoup(html, "html.parser")'''
+@bot.event
+async def on_ready():
+    print("bot logged in")
+    while not bot.is_closed():
+        await message()  # Run the Bot
+        await asyncio.sleep(3600) # Wait 1 hour to check for new tweets
 
-
+bot.run(os.getenv('DISCORD_TOKEN'))
