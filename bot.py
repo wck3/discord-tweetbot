6dfdb9829e0da05caa40a26240fdcbf1806b7571
Bill Kaminski <billykaminski123@gmail.com>
2023-07-07 10:51:34 -0400

    scraping logic complete

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import discord
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 from pprint import pprint
+import re
+import schedule
+import time
 
 
 # initialize an HTTP session
@@ -11,28 +14,30 @@ def go_to_bungo(url):
     res = session.get(url)
     soup = soup = BeautifulSoup(res.html.html, "html.parser")
     
-    
-    '''links = soup.find_all("a", {"class": "h4kbcd"})
-    for link in links:
-        #print(link, "\n")
-        res = session.get(link['href'])
-        soup = BeautifulSoup(res.html.html, "html.parser")
-        print(soup.find_all("a"), "\n")
-        
-        html = driver.page_source
-        soup = BeautifulSoup(html, "html.parser")'''
-
-   
-    divs_w_link = []
+    links = []
+    times = []
     divs = soup.find_all('div', {"class":"fy7gGf"})
     for div in divs:
         if div.find("a", {"class":"h4kbcd"}):
             #paragraphs = [p.text for p in soup.find_all('p')]
-            time = [d.text for d in div.find_all("span", {"class":"f"})]
-            time.sort()
-            print(time, "\n")
-            divs_w_link.append(div)
-   
+            time_tags = [d.text for d in div.find_all("span", {"class":"f"})]
+            regex = re.compile('.·')
+            #remove · character from time list, this is the only way I could think to remove it
+            time = [i for i in time_tags if not regex.match(i)]
+            #print(clean_time)
+            #print(clean_time[0][0])
+            print(time[0])
+            if 'hours' in time[0] and int(time[0][0]) > 1 or 'mins' in time[0]:
+                #times.append(time[0])
+                link = div.find("a")
+                links.append( link['href'] )
+            
+    
+    #times = list(set(times))
+    links = list(set(links))
+
+    #pprint(times)
+    #pprint(links)
     
     #pprint(res)
 
@@ -48,5 +53,24 @@ def go_to_bungo(url):
         print('We have logged in as {0.user}'.format(client))'''
     
 
+# Schedule the function call every hour
+schedule.every().hour.do( go_to_bungo('https://www.google.com/search?q=bungiehelp') )
 
-go_to_bungo('https://www.google.com/search?q=bungiehelp')
+# Run the scheduled task indefinitely
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
+    '''links = soup.find_all("a", {"class": "h4kbcd"})
+    for link in links:
+        #print(link, "\n")
+        res = session.get(link['href'])
+        soup = BeautifulSoup(res.html.html, "html.parser")
+        print(soup.find_all("a"), "\n")
+        
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")'''
+
+
